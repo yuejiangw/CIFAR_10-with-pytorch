17a9431ff468f2aff7869be6a4287f6144968346
226wyj <308033240@qq.com>
2021-04-09 12:03:25 +0800

    2021/4/9-12:02 add vgg-16 model and weight_decay

--- main.py
+++ main.py
@@ -23,20 +23,29 @@ def main(args):
     
     # 网络结构
     net = LeNet()
+    
     # 交叉熵损失函数
     criterion = nn.CrossEntropyLoss()
+    
     # SGD优化器
-    optimizer = optim.SGD(net.parameters(), lr=args.learning_rate, momentum=args.sgd_momentum)
+    optimizer = optim.SGD(
+        net.parameters(), 
+        lr=args.learning_rate, 
+        momentum=args.sgd_momentum, 
+        weight_decay=args.weight_decay
+    )
 
     
     # 模型的参数保存路径，默认为 "./model/state_dict"
     model_path = os.path.join(args.model_path, args.model_name)
 
+    # 启动训练
     if args.do_train:
         trainer = Trainer(net, criterion, optimizer, dataSet.train_loader, args)
         trainer.train(epochs=args.epoch)
         t.save(net.state_dict(), model_path)
     
+    # 启动测试
     if args.do_eval:
         if os.listdir(model_path) == []:
             print("Sorry, there's no saved model yet, you need to train first.")
@@ -50,25 +59,29 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+   
+    # 数据集
+    parser.add_argument("--num_workers", default=0, type=int, help="Thread number for training.")
+    parser.add_argument("--is_download", default=True, type=bool, help="Download the datasets if there is no data.")
 
     # 模型路径
     parser.add_argument("--model_path", default="./model", type=str, help="The directory of the saved model.")
     parser.add_argument("--model_name", default="state_dict", type=str, help="The name of the saved model's parameters.")
 
-
-    # 超参数
-    parser.add_argument("--num_workers", default=0, type=int, help="Thread number for training.")
-    parser.add_argument("--is_download", default=True, type=bool, help="Download the datasets if there is no data.")
-
+    # 训练相关
     parser.add_argument("--batch_size", default=4, type=int, help="Batch size for training and evaluation.")
+    parser.add_argument("--epoch", default=10, type=int, help="The number of training epochs.")
+    
+    # 超参数
     parser.add_argument("--learning_rate", default=0.001, type=float, help="The initial learning rate for Adam.")
+    parser.add_argument("--weight_decay", default=0.001, type=int, help="Weight decay of SGD optimzer.")
+    parser.add_argument("--sgd_momentum", default=0.8, type=float, help="The momentum of the SGD optimizer.")
+    
     parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="The Epsilon of Adam optimizer.")
     parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
     parser.add_argument("--dropout_rate", default=0.1, type=float, help="Dropout for fully-connected layers")
-    parser.add_argument("--epoch", default=10, type=int, help="The number of training epochs.")
-    parser.add_argument("--sgd_momentum", default=0.9, type=float, help="The momentum of the SGD optimizer.")
 
-    # bool值默认为false，当命令中包含如下参数时则bool值变为true
+    # 采取的动作
     parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
     parser.add_argument("--do_eval", action="store_true", help="Whether to run eval on the test set.")
     parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
--- model.py
+++ model.py
@@ -29,5 +29,78 @@ class LeNet(nn.Module):
         x = self.fc3(x)
         return x 
 
+class VGG16(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.num_classes = num_classes
+        # define an empty list for Conv_ReLU_MaxPool
+        net = []
+
+        # block 1
+        net.append(nn.Conv2d(in_channels=3, out_channels=64, padding=1, kernel_size=3, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.Conv2d(in_channels=64, out_channels=64, padding=1, kernel_size=3, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # block 2
+        net.append(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1))
+        net.append(nn.ReLU())
+        net.append(nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1))
+        net.append(nn.ReLU())
+        net.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # block 3
+        net.append(nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # block 4
+        net.append(nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # block 5
+        net.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1))
+        net.append(nn.ReLU())
+        net.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # add net into class property
+        self.extract_feature = nn.Sequential(*net)
+
+        # define an empty container for Linear operations
+        classifier = []
+        classifier.append(nn.Linear(in_features=512*7*7, out_features=4096))
+        classifier.append(nn.ReLU())
+        classifier.append(nn.Dropout(p=0.5))
+        classifier.append(nn.Linear(in_features=4096, out_features=4096))
+        classifier.append(nn.ReLU())
+        classifier.append(nn.Dropout(p=0.5))
+        classifier.append(nn.Linear(in_features=4096, out_features=self.num_classes))
+
+        # add classifier into class property
+        self.classifier = nn.Sequential(*classifier)
+
+
+    def forward(self, x):
+        feature = self.extract_feature(x)
+        feature = feature.view(x.size(0), -1)
+        classify_result = self.classifier(feature)
+        return classify_result
+
+
+
 class ResNet():
     pass
