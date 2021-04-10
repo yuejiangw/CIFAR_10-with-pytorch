7e502b399df769ac93b7b6217d9ea92f671aaea2
226wyj <308033240@qq.com>
2021-04-10 21:02:57 +0800

    predict

--- predict.py
+++ predict.py
@@ -14,8 +14,8 @@ class Predictor():
         transform = transforms.Compose([
             transforms.Resize(size=(32, 32)),
             transforms.ToTensor(),  # 转换成Tensor
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-            # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         ])
         return transform
 
